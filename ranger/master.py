eb1afd044b0b46f3465dee9142f95948403331b2
weichiu <none@none>
2014-11-10 17:20:24 -0500

    Create initial repo for the parkranger app (two-service evaluation)

---
+++ ranger/master.py
@@ -0,0 +1,191 @@
+#!/usr/bin/python2.7
+
+from optparse import OptionParser
+import logging
+import datetime
+import signal
+import sys
+
+import Utils
+
+logger = logging.getLogger('Benchmark.Master')
+
+
+def main(options):
+    """
+    Main module of master-microbenchmark.
+    """
+
+    # Read param file
+    param = Utils.param_reader(options.paramfile)
+
+    # Register for monitor
+    if options.monitor:
+        machines = Utils.get_machines(param["HOSTRUNFILE"], int(param["num_machines"]))
+        r = Utils.shell_exec('curl -s http://macesystems.cs.purdue.edu/manage/register/{}/{}/{} | tail -n1'.format(
+            param["USER"],
+            Utils.implode("-", machines),
+            param["run_time"]))
+        assert r.strip() == "SUCCESS", "Machine register failed. Resulted status is %s." % r
+
+
+    # Create directory for storing logs
+    log_name = datetime.datetime.now().strftime("%Y%m%d-%H-%M-%S")
+    log_dir = "%s/%s-%s" % (param["LOGDIR"], options.identifier, log_name)
+
+    Utils.mkdirp(log_dir)    
+
+    console_log = "%s/console.log" % log_dir
+    Utils.configureLogging('Benchmark', output_file=console_log)
+
+    logger.info("log_dir = %s", log_dir)
+
+    Utils.copyfile(log_dir, options.paramfile)
+    Utils.copyfile(log_dir, options.clientfile)
+    Utils.copyfile(log_dir, param["HOSTRUNFILE"])
+    Utils.copyfile(log_dir, param["HOSTNOHEADFILE"])
+    Utils.copyfile(log_dir, param["BOOTFILE"])
+
+    # Killall the experiment
+    cmd = 'killall python2.7 worker-run.py {binary}'.format(
+            binary=param["BINARY"])
+    Utils.shell_exec('{pssh_dir}/pssh -v -p {num_machines} -P -t 30 -h {hostfile} {command}'.format(
+        pssh_dir=param["PSSHDIR"],
+        num_machines=param["num_machines"], 
+        hostfile=param["HOSTRUNFILE"],
+        command=cmd))
+
+    # Clean up scratch directory
+    cmd = "'rm -rvf %s'" % param["SCRATCHDIR"]
+    Utils.shell_exec('{pssh_dir}/pssh -v -p {num_machines} -P -t 30 -h {hostfile} {command}'.format(
+        pssh_dir=param["PSSHDIR"],
+        num_machines=param["num_machines"], 
+        hostfile=param["HOSTRUNFILE"],
+        command=cmd))
+
+    # Sync the conf/* if needed
+    # Affected files are : boot hosts-run hosts-run-nohead params-run.conf
+    if param["SYNC_CONF_FILES"] == "1":
+        Utils.shell_exec('{pssh_dir}/pscp -v -h {hostfile} {bootfile} {confdir}'.format(
+            pssh_dir=param["PSSHDIR"],
+            hostfile=param["HOSTRUNFILE"],
+            bootfile=param["BOOTFILE"],
+            confdir=param["CONFDIR"]))
+
+        Utils.shell_exec('{pssh_dir}/pscp -v -h {hostfile} {hostfile} {confdir}'.format(
+            pssh_dir=param["PSSHDIR"],
+            hostfile=param["HOSTRUNFILE"],
+            confdir=param["CONFDIR"]))
+
+        Utils.shell_exec('{pssh_dir}/pscp -v -h {hostfile} {hostnoheadfile} {confdir}'.format(
+            pssh_dir=param["PSSHDIR"],
+            hostfile=param["HOSTRUNFILE"],
+            hostnoheadfile=param["HOSTNOHEADFILE"],
+            confdir=param["CONFDIR"]))
+
+        Utils.shell_exec('{pssh_dir}/pscp -v -h {hostfile} {conffile} {confdir}'.format(
+            pssh_dir=param["PSSHDIR"],
+            hostfile=param["HOSTRUNFILE"],
+            conffile=param["SERVER_CONFFILE"],
+            confdir=param["CONFDIR"]))
+
+        Utils.shell_exec('{pssh_dir}/pscp -v -h {hostfile} {conffile} {confdir}'.format(
+            pssh_dir=param["PSSHDIR"],
+            hostfile=param["HOSTRUNFILE"],
+            conffile=param["CLIENT_CONFFILE"],
+            confdir=param["CONFDIR"]))
+
+
+    # Run worker-run-microbenchmark.py with PSSH
+    # PSSH will be launched via fork() to catch Ctrl+C to stop the
+    # experiment.
+    cmd = '"ulimit -c unlimited; {bin}/worker-run.py -p {paramfile} -q {clientfile}"'.format(
+            bin=param["BIN"],
+            paramfile="%s/%s" % (param["BIN"], options.paramfile),
+            clientfile="%s/%s" % (param["BIN"], options.clientfile))
+    Utils.shell_launch('{pssh_dir}/pssh -v -p {num_machines} -P -t {run_time} -h {hostfile} {command}'.format(
+        pssh_dir=param["PSSHDIR"],
+        num_machines=param["num_machines"], 
+        run_time=int(param["run_time"]) + int(param["WORKER_JOIN_WAIT_TIME"]) + 
+                 int(param["CLIENT_WAIT_TIME"]) + int(param["TOTAL_BOOT_TIME"]) + 10,
+        hostfile=param["HOSTRUNFILE"],
+        command=cmd))
+
+    # Killall the experiment
+    cmd = 'killall python2.7 worker-run.py {binary} sar worker-sar.sh'.format(
+            binary=param["BINARY"])
+    Utils.shell_exec('{pssh_dir}/pssh -v -p {num_machines} -P -t 30 -h {hostfile} {command}'.format(
+        pssh_dir=param["PSSHDIR"],
+        num_machines=param["num_machines"], 
+        hostfile=param["HOSTRUNFILE"],
+        command=cmd))
+
+    # Copying logs
+    cmd = '{bin}/worker-log.py -p {paramfile} -l {logdir}'.format(
+            bin=param["BIN"],
+            paramfile="%s/%s" % (param["BIN"], options.paramfile),
+            logdir=log_dir)
+
+    Utils.shell_exec('ulimit -c unlimited; {pssh_dir}/pssh -v -p {num_machines} -P -t {run_time} -h {hostfile} {command}'.format(
+        pssh_dir=param["PSSHDIR"],
+        num_machines=param["num_machines"], 
+        run_time=param["run_time"],
+        hostfile=param["HOSTRUNFILE"],
+        command=cmd),
+        verbose=False)
+
+    # Adjust log directory permission
+    Utils.shell_exec("chmod -R a+rwx %s" % log_dir)
+
+    # Unregister for monitor
+    if options.monitor:
+        machines = Utils.get_machines(param["HOSTRUNFILE"], int(param["num_machines"]))
+        r = Utils.shell_exec('curl -s http://macesystems.cs.purdue.edu/manage/unregister/{}/{} | tail -n1'.format(
+            param["USER"],
+            Utils.implode("-", machines)))
+        assert r.strip() == "SUCCESS", "Machine register failed. Resulted status is %s." % r
+
+    # Done
+
+    logger.info("Done")
+
+
+###############################################################################
+# Main - Parse command line options and invoke main()
+if __name__ == "__main__":
+    parser = OptionParser(description="Microbenchmark experiment tool (master).")
+
+    parser.add_option("-a", "--application", dest="app", action="store", type="string",
+    help="Name of the application. e.g. microbenchmark.")
+    parser.add_option("-f", "--flavor", dest="flavor", action="store", type="string",
+    help="Type of the application. e.g. context.")
+    parser.add_option("-p", "--paramfile", dest="paramfile", action="store", type="string", 
+    help="Parameter file to run experiment.")
+    parser.add_option("-q", "--clientfile", dest="clientfile", action="store", type="string", 
+    help="Client parameter file to run experiment.")
+    parser.add_option("-i", "--identifier", dest="identifier", action="store", type="string", default="test",
+    help="Identifier of this experiments.")
+    parser.add_option("-m", "--monitor", dest="monitor", action="store_true",
+    help="Whether to register machines for status check.")
+
+
+    (options, args) = parser.parse_args()
+
+    # Check missing arguments
+
+    if not options.app:
+        parser.error("Missing --application")
+    if not options.flavor:
+        parser.error("Missing --flavor")
+    if not options.paramfile:
+        parser.error("Missing --paramfile")
+    if not options.clientfile:
+        parser.error("Missing --clientfile")
+
+    main_start_time = Utils.unixTime()
+
+    main(options)
+
+    main_end_time = Utils.unixTime()
+
+    logger.info("Total time : %f sec", main_end_time - main_start_time)
---
+++ ranger/run.sh
@@ -0,0 +1,228 @@
+#!/bin/bash
+# This is the script that runs multiple throughput benchmarks of the nacho runtime. 
+application="throughput"
+source ../common.sh
+
+mace_start_port=30000
+scale=8
+
+runtime=100 # duration of the experiment
+boottime=10   # total time to boot.
+
+tcp_nodelay=1   # If this is 1, you will disable Nagle's algorithm. It will provide better throughput in smaller messages.
+
+#nruns=1      # number of replicated runs
+nruns=5      # number of replicated runs
+
+flavor="nacho"
+#flavor="context"
+
+#context_policy="NO_SHIFT"
+#context_policy="SHIFT_BY_ONE"
+context_policy="RANDOM"
+
+# migration pattern parameters
+t_days=6
+day_period=40000000
+day_join=0.2
+day_leave=0.5
+day_error=0.2
+
+conf_client_file="conf/params-run-client.conf"
+conf_file="conf/params-run-server.conf"
+host_nohead_file="conf/hosts-run-nohead"
+
+if [ $# -eq 0 ]; then
+    id="default"
+else
+    id=$1
+fi
+
+# generate parameters for the benchmark. Parameters do not change in each of the benchmarks
+function GenerateBenchmarkParameter (){
+  conf_file=$1
+
+  echo "application = ${application}" >> ${conf_file}
+  echo "TOTAL_BOOT_TIME = ${boottime}" >> ${conf_file}
+
+  echo "HOSTNOHEADFILE = ${conf_dir}/hosts-run-nohead" >> $conf_file
+  echo "BINARY = ${application}_${flavor}" >> ${conf_file}
+  echo "flavor = ${flavor}" >> ${conf_file}
+  echo "MACE_START_PORT = ${mace_start_port}" >> ${conf_file}
+
+  echo "run_time = ${runtime}" >> ${conf_file}
+  echo "SET_TCP_NODELAY = ${tcp_nodelay}" >> ${conf_file}
+
+  echo "MACE_LOG_AUTO_SELECTORS = \"Accumulator GlobalStateCoordinator TcpTransport::connect BaseTransport::BaseTransport DefaultMappingPolicy ServiceComposition\"" >> ${conf_file}
+  echo "MACE_LOG_ACCUMULATOR = 1000" >> ${conf_file}
+
+  echo "WORKER_JOIN_WAIT_TIME = 40" >>  ${conf_file}
+  echo "CLIENT_WAIT_TIME = 20" >> ${conf_file}
+
+  echo "CONTEXT_ASSIGNMENT_POLICY = ${context_policy}" >> ${conf_file}
+
+  echo "SERVER_CONFFILE = ${conf_dir}/params-run-client.conf" >> $conf_file
+  echo "CLIENT_CONFFILE = ${conf_dir}/params-run-server.conf" >> $conf_file
+
+  echo "GRAPHVIZ_FILE = /tmp/gv.dot" >> $conf_file
+  if [[ $ec2 -eq 1 ]]; then
+    echo "EC2 = 1" >> ${conf_file}
+    echo "SYNC_CONF_FILES = 1" >> ${conf_file}
+  else
+    echo "EC2 = 0" >> ${conf_file}
+    echo "SYNC_CONF_FILES = 0" >> ${conf_file}
+  fi
+}
+
+function runexp (){
+  t_server_machines=$1
+  t_client_machines=$2
+  t_clients=$3
+  t_primes=$4
+
+  # For each server machine, you will run only one server process.
+  t_servers=$t_server_machines
+
+  #t_clients_per_machine=$(($t_clients/$t_client_machines))
+
+  # Get actual number of machines you will be using
+  t_machines=$(($t_server_machines + $t_client_machines + 1))
+
+  #for t_ncontexts in 24; do  # Number of total buildings across all the servers
+  t_scale=$(($t_server_machines+1))
+  t_ncontexts=$(($t_scale*6 ))
+
+  initial_server_size=$(($t_server_machines+1))
+
+  cp ${conf_orig_file} ${conf_file}
+
+  GenerateCommonParameter ${conf_file}
+  GenerateBenchmarkParameter ${conf_file}
+
+  # Generate parameters common to server and clients in this benchmark
+  echo "ServiceConfig.Throughput.NSENDERS = ${initial_server_size}" >>  ${conf_file}
+  echo "ServiceConfig.Throughput.NUM_PRIMES = ${t_primes}" >> ${conf_file}
+  echo "ServiceConfig.Throughput.NCONTEXTS = ${t_ncontexts}" >>  ${conf_file}
+
+  echo "num_machines = ${t_machines}" >> ${conf_file}
+  echo "num_server_machines = ${t_server_machines}" >> ${conf_file}
+  echo "num_client_machines = ${t_client_machines}" >> ${conf_file}
+  echo "num_servers = ${t_servers}" >> ${conf_file}
+  echo "num_clients = ${t_clients}" >> ${conf_file}
+  echo "num_contexts = ${t_ncontexts}" >> ${conf_file}
+  echo "day_period = ${day_period}" >> ${conf_file}
+  echo "day_join = ${day_join}" >> ${conf_file}
+  echo "day_leave = ${day_leave}" >> ${conf_file}
+  echo "day_error = ${day_error}" >> ${conf_file}
+  echo "num_days = ${t_days}" >> ${conf_file}
+
+  # copy the param file for clients
+  cp ${conf_file} ${conf_client_file}
+
+  echo -e "\n# Specific parameters for client" >> ${conf_client_file}
+
+  echo "ServiceConfig.Throughput.message_length = 1" >> ${conf_client_file}
+  echo "lib.MApplication.initial_size = 1" >> ${conf_client_file}
+  echo "ServiceConfig.Throughput.role = 1" >>  ${conf_client_file}
+  echo "MACE_LOG_AUTO_ALL = 0" >> ${conf_client_file}
+
+  # copy the param file for the server
+  echo -e "\n# Specific parameters for server" >> ${conf_file}
+
+  echo "lib.MApplication.services = Throughput" >> ${conf_file}
+  echo "lib.MApplication.initial_size = ${initial_server_size}" >> ${conf_file}
+  echo "ServiceConfig.Throughput.role = 2" >>  ${conf_file}
+  echo "MACE_LOG_AUTO_ALL = 0" >> ${conf_file}
+
+  # print out bootfile & param for servers
+  echo -e "\e[00;31m\$ ./configure.py -a ${application} -f ${flavor} -p ${mace_start_port} -o ${conf_file} -c ${conf_client_file} -i ${host_orig_file} -j ${host_run_file} -k ${host_nohead_file} -s ${boottime} -b ${boot_file}\e[00m"
+  ./configure.py -a ${application} -f ${flavor} -p ${mace_start_port} -o ${conf_file} -c ${conf_client_file} -i ${host_orig_file} -j ${host_run_file} -k ${host_nohead_file} -s ${boottime} -b ${boot_file}
+
+  if [[ $? -ne 0 ]]; then
+    echo "Error occurred while processing ./configure-${application}.py. Terminated."
+    exit 1;
+  fi
+
+  if [[ $ec2 -eq 0 ]]; then
+    # do not use monitor
+    echo -e "\e[00;31m\$ ./master.py -a ${application} -f ${flavor} -p ${conf_file} -q ${conf_client_file} -i n${t_server_machines}-m${t_client_machines}-s${t_servers}-c${t_clients}-b${t_ncontexts}-p${t_primes}\e[00m"
+    ./master.py -a ${application} -f ${flavor} -p ${conf_file} -q ${conf_client_file} -i ${application}-${flavor}-${id}-n${t_server_machines}-m${t_client_machines}-s${t_servers}-c${t_clients}-b${t_ncontexts}-p${t_primes}
+
+  else
+    # do not use monitor
+    #./master.py -a throughput -f context -p conf/params-run-server.conf -i n-c-p1-e-l
+    echo -e "\e[00;31m\$ ./master.py -a ${application} -f ${flavor} -p ${conf_file} -i n${t_nodes}-c${t_contexts}-p${t_primes}-e${total_events}-l${t_payload}\e[00m"
+    ./master.py -a ${application} -f ${flavor} -p ${conf_file} -q ${conf_client_file} -i ${application}-${flavor}-${id}-n${t_server_machines}-m${t_client_machines}-s${t_servers}-c${t_clients}-b${t_ncontexts}-p${t_primes}
+  fi
+  sleep 10
+
+}
+
+function aggregate_output () {
+  log_set_dir=$1
+  t_clients=$2
+  t_primes=$3
+  # create a new directory for the set of logs
+  #log_set_dir=`date --iso-8601="seconds"`
+  mkdir ${logdir}/${log_set_dir}
+  # move the log directories into the new dir
+  mv ${logdir}/${application}-* ${logdir}/${log_set_dir}/
+  # parse the logs in each of the log directory, and aggregate the throughput
+  # measure throughput from 10% to 90% (assuming the throughput is stable in the period)
+  # compute average and standard deviation
+  # append to the output file
+  cwd=`pwd`
+  cd log
+  ./run-throughput.sh ${logdir}/${log_set_dir} $flavor-$t_clients-$t_primes
+  cd $cwd
+}
+
+function init() {
+  # create directories on all nodes
+  ${psshdir}/pssh -h conf/hosts -t 30 mkdir -p $scratchdir
+  #if [[ $ec2 -eq 1 ]]; then
+  #else
+
+  #fi
+}
+
+init
+
+for t_server_machines in 7; do
+  for t_client_machines in  4; do
+    for t_clients in 8; do
+      for t_primes in 1; do  # Additional computation payload at the server.
+        log_set_dir=`date --iso-8601="seconds"`
+        for (( run=1; run <= $nruns; run++ )); do
+          mace_start_port=$((mace_start_port+500))
+          runexp $t_server_machines $t_client_machines $t_clients $t_primes
+
+          # generate plots
+          cwd=`pwd`
+          cd log
+          ./plot_connection.sh ${t_server_machines}-${t_clients}-$run
+          ./run-timeseries.sh
+          cd $cwd
+          # publish plots and parameters and logs to web page
+          #if [[ $ec2 -eq 0 ]]; then
+            ./publish.sh $log_set_dir
+          #fi
+        done # end of nruns
+
+        #TODO: compute avg and stddev, and plot error bar.
+        # Find the last $nruns log, aggregate the compute/plot error bar
+
+        # what to plot? the average throughput w/ error
+        aggregate_output $log_set_dir $t_clients $t_primes 
+        cwd=`pwd`
+        cd log
+        ./run-avg.sh
+        ./plot_service.sh
+        cd $cwd
+
+        ./publish_webindex.sh $log_set_dir
+      done # end of total_events
+    done
+  done
+done
+
